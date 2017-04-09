2973e8b42bc376d09127de16d7647cd1a2e514ee
Oliv4945 <oliv4945@gmail.com>
2017-04-10 01:00:57 +0200

    Add preferencies pannel

--- app/src/main/java/net/iopush/jarvis/MainActivity.java
+++ app/src/main/java/net/iopush/jarvis/MainActivity.java
@@ -1,5 +1,7 @@
 package net.iopush.jarvis;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.Locale;
@@ -10,6 +12,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -30,12 +33,17 @@ import java.net.URLEncoder;
 
 import android.speech.tts.TextToSpeech;
 
+import 	android.support.v7.widget.Toolbar;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtSpeechInput;
     private ImageButton btnSpeak;
     private final int REQ_CODE_SPEECH_INPUT = 100;
     TextToSpeech ttsEngine;
+
+    private String serverUrl;
+    private String serverPort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +53,8 @@ public class MainActivity extends AppCompatActivity {
         txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
 
-        // hide the action bar
-        getSupportActionBar().hide();
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         btnSpeak.setOnClickListener(new View.OnClickListener() {
 
@@ -62,11 +70,29 @@ public class MainActivity extends AppCompatActivity {
             public void onInit(int status) {
                 if(status != TextToSpeech.ERROR) {
                     // TODO - https://developer.android.com/reference/android/speech/tts/TextToSpeech.html#isLanguageAvailable(java.util.Locale)
-                    ttsEngine.setLanguage(Locale.FRENCH);
+                    ttsEngine.setLanguage(Locale.getDefault());
                 }
             }
         });
 
+        // Get preferences
+        SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        serverUrl = SP.getString("serverUrl", "NA");
+        serverPort = SP.getString("serverPort", "NA");
+        if (serverUrl == "NA") {
+            Intent i = new Intent(this, MyPreferencesActivity.class);
+            startActivity(i);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+
+        // TODO : Better to call pref change listener
+        SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        serverUrl = SP.getString("serverUrl", "NA");
+        serverPort = SP.getString("serverPort", "NA");
     }
 
     /**
@@ -104,9 +130,10 @@ public class MainActivity extends AppCompatActivity {
                     txtSpeechInput.setText(result.get(0));
                     Log.i("Jarvis", "STT: " + result.get(0));
 
+
                     // Instantiate the RequestQueue.
                     RequestQueue queue = Volley.newRequestQueue(this);
-                    String requestUrl = "https://iopush.net:8080/?order=" + URLEncoder.encode(result.get(0));
+                    String requestUrl = serverUrl+":"+serverPort+"/?order=" + URLEncoder.encode(result.get(0));
 
                     // Request a string response from the provided URL.
                     StringRequest stringRequest = new StringRequest(Request.Method.GET, requestUrl,
@@ -156,7 +183,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        // getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent i = new Intent(this, MyPreferencesActivity.class);
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
