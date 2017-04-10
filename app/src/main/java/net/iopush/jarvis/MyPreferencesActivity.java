f0aea19c9d5e5b2eeaabbee4f83fc70a5c9c87ec
Oliv4945 <oliv4945@gmail.com>
2017-04-10 21:12:33 +0200

    Add a return button in preferences

--- app/src/main/java/net/iopush/jarvis/MyPreferencesActivity.java
+++ app/src/main/java/net/iopush/jarvis/MyPreferencesActivity.java
@@ -4,12 +4,23 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 
-public class MyPreferencesActivity extends PreferenceActivity {
+import android.support.v7.app.AppCompatActivity;
+
+public class MyPreferencesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 
     public static class MyPreferenceFragment extends PreferenceFragment
